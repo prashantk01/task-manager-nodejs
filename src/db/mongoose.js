d49fa0f8f087decef43eed4ad9166f8a9be646fe
Prashant Kumar <kumarprince2510@gmail.com>
2022-08-20 16:55:10 +0530

    rest-api: created api hanles client side get & put request

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,33 +1,15 @@
 const mongoose = require('mongoose')
 
+
+/**
+ * connectin to db with provided url
+ */
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-nodejs', {
     useNewUrlParser: true,
     // below code is umsupported for vmongodb > 6
     // useCreateIndex: true 
 }).then(() => {
-    console.log("database connected")
+    console.log("database connected !! ")
 }).catch((e) => {
     console.log("we got error: "+e)
-})
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task = new Task({
-    description: '  Sleep time'
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
 })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user');
-
+const Task = require("./models/task")
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,6 +16,36 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e)
     });
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        console.log(tasks);
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/task/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task)
+            res.send(404).send(task)
+        console.log(task);
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
 })
 
 
