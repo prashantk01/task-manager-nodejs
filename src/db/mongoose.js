034468ea0e7c1d293e0a3a99ed261a63405b2f20
Prashant Kumar <kumarprince2510@gmail.com>
2022-08-21 10:24:59 +0530

    hashing: npm bcryptjs used for password hashing & schema refactor

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+// const User = require("./../models/user");
 
 /**
  * connectin to db with provided url
@@ -13,3 +13,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-nodejs', {
 }).catch((e) => {
     console.log("we got error: "+e)
 })
+
+// const user = {
+//     name: " prince",
+//     email: "test1@gmail.com",
+//     password: "test1@123",
+//     age: 21
+// };
+
+// new User(user).save();
