29c6dbb132be977c2d4d21ae003c6970a7bd8e1f
Prashant Kumar <kumarprince2510@gmail.com>
2022-08-28 12:13:51 +0530

    auhtorizting one route with jwtwebtoken

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 
-router.post('/users', async (req, res) => {
+router.post('/users', async (req, res) =>{
     const user = new User(req.body)
 
     try {
@@ -31,13 +32,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
