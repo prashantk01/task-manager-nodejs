795ab1a4cecc078b08b6ccf2943d2d4fe35f8c67
Prashant Kumar <kumarprince2510@gmail.com>
2022-08-29 12:37:15 +0530

    logout: logout api that deletes current jwtwebtoken from user schema and logout user

--- src/routers/user.js
+++ src/routers/user.js
@@ -32,6 +32,34 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout',auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.status(200).send()
+    }
+    catch (e) {
+         res.status(500).send()
+
+    }
+})
+
+/**
+ * this methods logout current user from all logged in sessions
+ */
+router.post('/users/logoutAll',auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.status(200).send()
+    }
+    catch (e) {
+        res.status(500).send()
+    }
+})
+
 /**
  * @auth express middleware for authorization
  */
