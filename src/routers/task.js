7f2a4a55189c0a5045fc17caee1ae7bc109f57c5
Prashant Kumar <kumarprince2510@gmail.com>
2022-08-29 22:08:13 +0530

    skip & limit on db query

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,11 +17,22 @@ router.post('/tasks',auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', auth , async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
+    const macth = {}
+    if (req.query.completed) {
+        match.completed= req.query.completed === "true";
+    }
     try {
         // const tasks = await Task.find({owner:req.user._id})
         // or
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send(e)
